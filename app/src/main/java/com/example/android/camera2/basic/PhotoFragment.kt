3fce0fdce559c14bc5b49d85dfc73d6f78db5bcd
Steven Wilson <Steven.Wilson@netally.com>
2022-11-11 09:52:37 -0700

    Implement photo view and share/delete buttons

--- app/src/main/java/com/example/android/camera2/basic/PhotoFragment.kt
+++ app/src/main/java/com/example/android/camera2/basic/PhotoFragment.kt
@@ -1,20 +1,25 @@
 package com.example.android.camera2.basic
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.android.camera2.basic.fragments.CameraFragmentArgs
+import com.bumptech.glide.Glide
 
 /**
- * A simple [Fragment] subclass.
- * Use the [PhotoFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * A simple image viewer fragment with delete and share buttons
  */
 class PhotoFragment : Fragment() {
+    private val TAG = "PhotoFragment"
     private val args: PhotoFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -27,12 +32,40 @@ class PhotoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textView = requireActivity().findViewById<TextView>(R.id.photo_filename)
-        val photoUriString = args.imageUri
+        val photoView = requireActivity().findViewById<ImageView>(R.id.photo_image)
+        val shareButton = requireActivity().findViewById<ImageButton>(R.id.share_button)
+        val deleteButton = requireActivity().findViewById<ImageButton>(R.id.delete_button)
+        val photoUri = Uri.parse(args.imageUri)
 
-        textView.text = photoUriString
-    }
 
-    companion object {
+        shareButton.setOnClickListener {
+            val photoUri = Uri.parse(args.imageUri)
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                putExtra(Intent.EXTRA_STREAM, photoUri)
+                setDataAndType(photoUri, "image/jpeg")
+
+            }
+            try {
+                startActivity(sendIntent)
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to start share activity")
+            }
+        }
+
+        deleteButton.setOnClickListener {
+            try {
+                requireActivity().contentResolver.delete(photoUri, null, null)
+                findNavController().popBackStack()
+            } catch (e: Exception) {
+                Log.e(TAG, "Failed to delete image $photoUri")
+            }
+        }
+
+        Glide
+            .with(this)
+            .load(photoUri)
+            .into(photoView)
     }
 }
